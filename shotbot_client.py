824f0f1e2352faf73a150d9f5c4618cacf785e47
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-14 16:16:27 -0400

    Added command timer and expanded logic

--- shotbot_client.py
+++ shotbot_client.py
@@ -1,6 +1,7 @@
 import discord
 import logging
 import os
+import time
 from dotenv import load_dotenv
 
 client = discord.Client()
@@ -12,14 +13,12 @@ logger.addHandler(handler)
 
 load_dotenv()
 logger.debug("Used load_dotenv() to load environment")
+
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD_NAME = os.getenv('GUILD_NAME')
 SPECIFIED_CHANNEL_NAME = os.getenv('SPECIFIED_CHANNEL_NAME')
 
 command_prefix = "$"
-
-def getCommandFor(input_string):
-    return command_prefix+input_string
 
 command_list = {
     "help": "help",
@@ -28,21 +27,61 @@ command_list = {
     "fill": "punish"
 }
 
+def getCommandFor(input_string):
+    return command_prefix+command_list[input_string]
+
 command_help = {
-    getCommandFor(command_list["help"]: "Display help regarding Discord-ShotBot",
+    getCommandFor(command_list["help"]: "Display help regarding Discord-ShotBot (That's this)",
     getCommandFor(command_list["about"]: "Why was an abomination like me created?",
-    getCommandFor(command_list["pour"]: "Pour Tekktrik a glass",
-    getCommandFor(command_list["fill"]: "He won't learn otherwise"
+    getCommandFor(command_list["pour"]: "Pour {0.user} a glass".format(client),
+    getCommandFor(command_list["fill"]: "{0.user} won't learn otherwise".format(client)
 }
 
-command_help
-
 class CommandTimer:
-
-    def __init__(self, id_string):
     
-        self._id = id_string
-        self._unlock_command = time.time()
+    def __init__(self, single_lockout=10, fill_lockout=60):
+    
+        self._single_lockout_sec = single_lockout*60
+        self._fill_lockout_sec = fill_lockout*60
+        self._single_unlock_time = time.time()
+        self._fill_unlock_time = time.time()
+        self._in_progress_lock = False
+        self._user_lock = False
+        
+    def isSingleTimerReady(self):
+        return True if (time.time() >= self._single_unlock_time) else False
+        
+    def isFillTimerReady(self):
+        return True if (time.time() >= self._fill_unlock_time) else False
+        
+    def resetTimers(self):
+        self._single_unlock_time = time.time() + self._single_lockout_sec
+        self._fill_unlock_time = time.time() + self._fill_lockout_sec
+        
+    def getTimerTimeRemaining(self):
+        single_time_remain = self._single_unlock_time - time.time()
+        fill_time_remain = self._fill_unlock_time - time.time()
+        return single_time_remain, fill_time_remain
+        
+    def setInProgress(self):
+        self._in_progress_lock = True
+        
+    def markProgressComplete(self):
+        self._in_progress_lock = False
+        
+    def isInProgress(self):
+        return self._in_progress_lock
+        
+    def setUserLockOn(self):
+        self._user_lock = True
+        
+    def setUserLockOff(self):
+        self._user_lock = False
+        
+    def isUserLockSet(self):
+        return self._user_lock
+        
+command_timer = CommandTimer()
 
 @client.event
 async def on_ready():
@@ -64,23 +103,55 @@ async def on_message(message):
     if message.channel.name != SPECIFIED_CHANNEL_NAME:
         return
         
+    is_pour_command = (message.content == getCommandFor("pour"))
+    is_fill_command = (message.content == getCommandFor("fill"))
+        
     if message.content == getCommandFor("about"):
         logger.info("{0.author} asked why the hell this even exsits".format(message))
         await message.channel.send(
             "Fuck you, that's why <3\n\
             Built with love and too much time"
         )
+    
+    elif (is_pour_command or is_fill_command):
+        logger.debug("Pouring command registered")
         
-    elif message.content == getCommandFor("pour"):
-        logger.info("{0.author} attempted to pour {1.user} shot".format(message, client))
-        # pour a shot glass
+        if command_timer.isInProgress():
+            single_timer_remaining, fill_timer_remaining = command_timer.getTimerTimeRemaining()
+            single_remaining_str = str(int(single_timer_remaining))+":"+str(round((single_timer_remaining-int(single_timer_remaining))*60))
+            fill_remaining_str = str(int(fill_timer_remaining))+":"+str(round((fill_timer_remaining-int(fill_timer_remaining))*60))
+            logger.info("Damn, {0.author} really wanted to make {1.user} drink, but the timer isn't up!".format(message, client))
+            logger.info("Time remaining for single pour: {}".format(single_remaining_str))
+            logger.info("Time remaining for full pour: {}".format(fill_remaining_str))
+            choose_time_remaining = single_remaining_str if is_pour_command else fill_remaining_str
+            await message.channel.send("Woah, woah, woah - slow down there, {0.author}!  {1.user} still has about {3} left before you can do that!".format(message, client, choose_time_remaining))
+            return
+            
+        if command_timer.isUserLockSet():
+            logger.info("User lockout is set, will not pour shot(s)")
+            await message.channel.send("Sorry, {0.author}!  Looks like {1.user} isn't ready for another one yet!".format(message, client))
+            return
         
-    elif message.content == getCommandFor("fill"):
-        logger.info("{0.author} attempted to punish {1.user}, what a jerk".format(message, client))
-        # pour all glasses
+        if is_pour_command:
+            logger.info("{0.author} attempted to pour {1.user} shot".format(message, client))
+            # pour a shot glass
+            
+        else:
+            logger.info("{0.author} attempted to punish {1.user}, what a jerk".format(message, client))
+            # pour all glasses
+        
+    elif message.content == getCommandFor("help"):
+        logger.info("{0.author} asked for help".format(message))
+        help_message = ""
+        for command, help_text in command_help.items():
+            help_message += "".join(command, ": ", help_text, "\n")
+        help_message += "".join("\n", "Created by Tekktrik using Python")
+        await message.channel.send(help_message)
         
     elif message.content.startswith(command_prefix):
-        await message.channel.send("Beep boop, does not compute.  Try using $help command.")
         logger.info("{0.author} seemingly tried to use an invalid command".format(message))
+        await message.channel.send("Beep boop, does not compute.  Try using $help command.")
+    
+    return
         
 client.run(DISCORD_TOKEN)
