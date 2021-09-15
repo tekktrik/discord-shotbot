ccebd5be7a2cad421f1e4b2e37f4c4421082359e
tekktrik <89490472+tekktrik@users.noreply.github.com>
2021-09-14 22:17:43 -0400

    Revert "Removed .user calls in format()"
    This reverts commit 56782720d1c72b9010b4ab6300c61c526fd8543b.

--- shotbot_client.py
+++ shotbot_client.py
@@ -42,8 +42,8 @@ def getCommandFor(input_string):
 command_help = {
     getCommandFor("help"): "Display help regarding Discord-ShotBot (That's this)",
     getCommandFor("about"): "Why was an abomination like me created?",
-    getCommandFor("pour"): "Pour {0} a glass".format(shot_recepient),
-    getCommandFor("fill"): "{0} won't learn otherwise".format(shot_recepient)
+    getCommandFor("pour"): "Pour {0.user} a glass".format(shot_recepient),
+    getCommandFor("fill"): "{0.user} won't learn otherwise".format(shot_recepient)
 }
 
 class CommandTimer:
@@ -127,7 +127,7 @@ async def on_message(message):
             single_timer_remaining, fill_timer_remaining = command_timer.getTimerTimeRemaining()
             single_remaining_str = str(int(single_timer_remaining))+":"+str(round((single_timer_remaining-int(single_timer_remaining))*60))
             fill_remaining_str = str(int(fill_timer_remaining))+":"+str(round((fill_timer_remaining-int(fill_timer_remaining))*60))
-            logger.info("Damn, {0.author} really wanted to make {1} drink, but the timer isn't up!".format(message, shot_recepient))
+            logger.info("Damn, {0.author} really wanted to make {1.user} drink, but the timer isn't up!".format(message, shot_recepient))
             logger.info("Time remaining for single pour: {}".format(single_remaining_str))
             logger.info("Time remaining for full pour: {}".format(fill_remaining_str))
             choose_time_remaining = single_remaining_str if is_pour_command else fill_remaining_str
@@ -136,15 +136,15 @@ async def on_message(message):
             
         if command_timer.isUserLockSet():
             logger.info("User lockout is set, will not pour shot(s)")
-            await message.channel.send("Sorry, {0.author}!  Looks like {1} isn't ready for another one yet!".format(message, shot_recepient))
+            await message.channel.send("Sorry, {0.author}!  Looks like {1.user} isn't ready for another one yet!".format(message, shot_recepient))
             return
         
         if is_pour_command:
-            logger.info("{0.author} attempted to pour {1} shot".format(message, shot_recepient))
+            logger.info("{0.author} attempted to pour {1.user} shot".format(message, shot_recepient))
             # pour a shot glass
             
         else:
-            logger.info("{0.author} attempted to punish {1}, what a jerk".format(message, shot_recepient))
+            logger.info("{0.author} attempted to punish {1.user}, what a jerk".format(message, shot_recepient))
             # pour all glasses
         
     elif message.content == getCommandFor("help"):
